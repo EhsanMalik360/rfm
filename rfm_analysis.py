1a6b973de32dfaef701a7b20da051c8d5511198b
EhsanMalik360 <leverify.bi022@gmail.com>
2023-11-15 10:07:58 +0500

    rfm according to moengage

--- rfm_analysis.py
+++ rfm_analysis.py
@@ -3,77 +3,93 @@ import pandas as pd
 from datetime import datetime
 
 
-# Function to try multiple encodings
-def load_data(uploaded_file):
-    for encoding in ['utf-8', 'ISO-8859-1', 'latin1']:
-        try:
-            return pd.read_csv(uploaded_file, encoding=encoding)
-        except UnicodeDecodeError:
-            pass
-    raise UnicodeDecodeError("Unable to find the correct encoding. Please check your file.")
+# Function to calculate RFM metrics
+def calculate_rfm(data):
+    # Convert InvoiceDate to datetime
+    data['InvoiceDate'] = pd.to_datetime(data['InvoiceDate'])
+
+    # Calculate Recency, Frequency, and Monetary metrics
+    max_date = data['InvoiceDate'].max() + pd.to_timedelta(1, unit='d')
+    rfm = data.groupby('CustomerID').agg({
+        'InvoiceDate': lambda x: (max_date - x.max()).days,
+        'CustomerID': 'count',
+        'TotalSpend': 'sum'
+    }).rename(columns={'InvoiceDate': 'Recency', 'CustomerID': 'Frequency', 'TotalSpend': 'Monetary'})
+
+    # Return RFM DataFrame
+    return rfm
 
 
-# Set title of the app
-st.title('RFM Analysis Tool')
+# Function to segment customers based on RFM scores
+def segment_customers(rfm_data):
+    # Quartile-based segmentation
+    quartiles = rfm_data.quantile(q=[0.25, 0.5, 0.75])
 
-# File uploader widget
-uploaded_file = st.file_uploader("Upload your CSV", type="csv")
-if uploaded_file is not None:
-    try:
-        # Try loading the data with different encodings
-        data = load_data(uploaded_file)
+    # Function to assign R, F, M segments
+    def RScore(x, p, d):
+        if x <= d[p][0.25]:
+            return 1
+        elif x <= d[p][0.50]:
+            return 2
+        elif x <= d[p][0.75]:
+            return 3
+        else:
+            return 4
 
-        # Display the dataframe
-        st.write('Raw data')
-        st.write(data)
+    def FMScore(x, p, d):
+        if x <= d[p][0.25]:
+            return 4
+        elif x <= d[p][0.50]:
+            return 3
+        elif x <= d[p][0.75]:
+            return 2
+        else:
+            return 1
 
-        # User input for unique identifier, date, and monetary value columns
-        id_col = st.selectbox('Select Customer ID Column', data.columns)
-        date_col = st.selectbox('Select Invoice Date Column', data.columns)
-        money_col = st.selectbox('Select Monetary Value Column', data.columns)
+    rfm_data['R_Quartile'] = rfm_data['Recency'].apply(RScore, args=('Recency', quartiles))
+    rfm_data['F_Quartile'] = rfm_data['Frequency'].apply(FMScore, args=('Frequency', quartiles))
+    rfm_data['M_Quartile'] = rfm_data['Monetary'].apply(FMScore, args=('Monetary', quartiles))
 
-        # Process the data when 'Analyze' button is clicked
-        if st.button('Analyze'):
-            # Convert date column to datetime
-            data[date_col] = pd.to_datetime(data[date_col])
+    # Combine RFM scores
+    rfm_data['RFM_Score'] = rfm_data['R_Quartile'].map(str) + rfm_data['F_Quartile'].map(str) + rfm_data[
+        'M_Quartile'].map(str)
 
-            # Calculate Recency, Frequency, Monetary values for each customer
-            latest_date = data[date_col].max() + pd.DateOffset(days=1)
-            rfm = data.groupby(id_col).agg({
-                date_col: lambda x: (latest_date - x.max()).days,
-                id_col: 'count',
-                money_col: 'sum'
-            })
+    # Segmentation labels (customize as needed)
+    segmentation_map = {
+        '111': 'Best Customers',
+        '411': 'New Customers',
+        '444': 'Lost Cheap Customers',
+        # Add more segments as needed
+    }
+    rfm_data['Segment'] = rfm_data['RFM_Score'].map(segmentation_map)
 
-            # Rename the columns
-            rfm.rename(columns={date_col: 'Recency',
-                                id_col: 'Frequency',
-                                money_col: 'MonetaryValue'}, inplace=True)
-
-
-            # RFM scoring
-            def get_qcut(series, labels, n_quantiles=4):
-                unique_vals = series.nunique()
-                if unique_vals < n_quantiles:
-                    return pd.cut(series, bins=unique_vals, labels=labels[:unique_vals], duplicates='drop')
-                else:
-                    return pd.qcut(series, q=n_quantiles, labels=labels, duplicates='drop')
+    return rfm_data
 
 
-            rfm['R'] = get_qcut(rfm['Recency'], ['1', '2', '3', '4'])
-            rfm['F'] = get_qcut(rfm['Frequency'], ['4', '3', '2', '1'])
-            rfm['M'] = get_qcut(rfm['MonetaryValue'], ['4', '3', '2', '1'])
+# Streamlit app
+def main():
+    st.title("RFM Analysis App")
 
-            rfm['RFM_Segment'] = rfm['R'].astype(str) + rfm['F'].astype(str) + rfm['M'].astype(str)
-            rfm['RFM_Score'] = rfm[['R', 'F', 'M']].sum(axis=1)
+    # File uploader
+    uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+    if uploaded_file is not None:
+        # Read data
+        data = pd.read_csv(uploaded_file)
 
-            # Display RFM segmentation
-            st.write('RFM Analysis')
-            st.write(rfm)
+        # Calculate RFM
+        rfm_data = calculate_rfm(data)
 
-            # Optional: Display RFM segments distribution
-            st.bar_chart(rfm['RFM_Segment'].value_counts())
-    except UnicodeDecodeError as e:
-        st.error(f'Error reading file: {e}')
-else:
-    st.info('Awaiting CSV file to be uploaded.')
+        # Segment customers
+        segmented_data = segment_customers(rfm_data)
+
+        # Display results
+        st.write("RFM Analysis Results")
+        st.dataframe(segmented_data)
+
+        # Export button (optional)
+        st.download_button(label="Download RFM Data", data=segmented_data.to_csv(), file_name='rfm_analysis.csv',
+                           mime='text/csv')
+
+
+if __name__ == "__main__":
+    main()
