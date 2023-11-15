094a70a8967332bd83aea8e6c327405acb8b5153
EhsanMalik360 <leverify.bi022@gmail.com>
2023-11-15 13:00:55 +0500

    old version validation not applied.

--- rfm_analysis.py
+++ rfm_analysis.py
@@ -1,38 +1,77 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import datetime
+
 
 # Function to calculate RFM metrics
+# Function to calculate RFM metrics
 def calculate_rfm(data, date_col, id_col, spend_col):
-    try:
-        data[date_col] = pd.to_datetime(data[date_col])
-    except Exception as e:
-        st.error(f"Error converting Invoice Date column to datetime: {e}")
-        return None
+    # Convert InvoiceDate to datetime
+    data[date_col] = pd.to_datetime(data[date_col])
 
+    # Calculate Recency, Frequency, and Monetary metrics
     max_date = data[date_col].max() + pd.to_timedelta(1, unit='d')
     rfm = data.groupby(id_col).agg({
         date_col: lambda x: (max_date - x.max()).days,
         spend_col: 'sum'
     }).rename(columns={date_col: 'Recency', spend_col: 'Monetary'})
     rfm['Frequency'] = data.groupby(id_col)[id_col].count()
+
+    # Reset index to keep CustomerID in the DataFrame
     rfm.reset_index(inplace=True)
+
     return rfm
+
+
 
 # Function to segment customers based on RFM scores
 def segment_customers(rfm_data):
+    # Quartile-based segmentation
     quartiles = rfm_data.quantile(q=[0.25, 0.5, 0.75])
+
+    # Function to assign R, F, M segments
+    def RScore(x, p, d):
+        if x <= d[p][0.25]:
+            return 1
+        elif x <= d[p][0.50]:
+            return 2
+        elif x <= d[p][0.75]:
+            return 3
+        else:
+            return 4
+
+    def FMScore(x, p, d):
+        if x <= d[p][0.25]:
+            return 4
+        elif x <= d[p][0.50]:
+            return 3
+        elif x <= d[p][0.75]:
+            return 2
+        else:
+            return 1
+
     def RScore(x, p, d): return 4 if x <= d[p][0.25] else 3 if x <= d[p][0.50] else 2 if x <= d[p][0.75] else 1
     def FMScore(x, p, d): return 1 if x <= d[p][0.25] else 2 if x <= d[p][0.50] else 3 if x <= d[p][0.75] else 4
     rfm_data['R_Quartile'] = rfm_data['Recency'].apply(RScore, args=('Recency', quartiles))
     rfm_data['F_Quartile'] = rfm_data['Frequency'].apply(FMScore, args=('Frequency', quartiles))
     rfm_data['M_Quartile'] = rfm_data['Monetary'].apply(FMScore, args=('Monetary', quartiles))
+
+    # Combine RFM scores
+    rfm_data['RFM_Score'] = rfm_data['R_Quartile'].map(str) + rfm_data['F_Quartile'].map(str) + rfm_data[
+        'M_Quartile'].map(str)
+
+    # Segmentation labels (customize as needed)
     rfm_data['RFM_Score'] = rfm_data['R_Quartile'].map(str) + rfm_data['F_Quartile'].map(str) + rfm_data['M_Quartile'].map(str)
     segmentation_map = {
+        '111': 'Best Customers',
+        '411': 'New Customers',
+        '444': 'Lost Cheap Customers',
         '111': 'Best Customers', '411': 'New Customers', '444': 'Lost Cheap Customers',
         # Add more segments as needed
     }
     rfm_data['Segment'] = rfm_data['RFM_Score'].map(segmentation_map)
+
     return rfm_data
 
 # Function to plot segment distribution
@@ -46,52 +85,42 @@ def plot_segment_distribution(segmented_data):
     plt.xticks(rotation=45)
     return plt
 
-# Function to check data types and convert if possible
-def validate_data_types(data, date_col, id_col, spend_col):
-    if not pd.api.types.is_numeric_dtype(data[spend_col]):
-        try:
-            data[spend_col] = pd.to_numeric(data[spend_col])
-        except ValueError:
-            return "Total Spend column should be numeric or convertible to numeric."
-
-    if data[id_col].dtype != 'O':
-        data[id_col] = data[id_col].astype(str)
-
-    return None
-
 # Streamlit app main function
 def main():
     st.title("RFM Analysis App")
 
+    # File uploader
     uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
     if uploaded_file is not None:
         data = pd.read_csv(uploaded_file)
 
+        # Column selection
         st.write("Select the columns for RFM analysis")
         id_col = st.selectbox("Select Customer ID Column", data.columns)
         date_col = st.selectbox("Select Invoice Date Column", data.columns)
         spend_col = st.selectbox("Select Total Spend Column", data.columns)
 
         if st.button("Calculate RFM"):
-            error_message = validate_data_types(data, date_col, id_col, spend_col)
-            if error_message:
-                st.error(error_message)
-            else:
-                rfm_data = calculate_rfm(data, date_col, id_col, spend_col)
-                if rfm_data is not None:
-                    segmented_data = segment_customers(rfm_data)
+            # Calculate RFM
+            rfm_data = calculate_rfm(data, date_col, id_col, spend_col)
 
-                    st.write("RFM Analysis Results")
-                    st.dataframe(segmented_data)
+            # Segment customers
+            segmented_data = segment_customers(rfm_data)
 
-                    st.write("Segment Distribution")
-                    fig = plot_segment_distribution(segmented_data)
-                    st.pyplot(fig)
+            # Display results
+            st.write("RFM Analysis Results")
+            st.dataframe(segmented_data)
 
-                    st.download_button(label="Download RFM Data",
-                                       data=segmented_data.to_csv(index=False),
-                                       file_name='rfm_analysis.csv',
-                                       mime='text/csv')
+            # Export button (optional)
+            st.download_button(label="Download RFM Data", data=segmented_data.to_csv(index=False),
+                               file_name='rfm_analysis.csv', mime='text/csv')
+            # Plotting segment distribution
+            st.write("Segment Distribution")
+            fig = plot_segment_distribution(segmented_data)
+            st.pyplot(fig)
+
+            # Export button
+            st.download_button(label="Download RFM Data", data=segmented_data.to_csv(index=False), file_name='rfm_analysis.csv', mime='text/csv')
 
 if __name__ == "__main__":
     main()
